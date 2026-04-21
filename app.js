f231969cea02a380b28615e0b1ffb43caf4d7158
ssiga254 <ssiga254@gmail.com>
2026-04-21 10:23:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,140 @@
+let currentFilter = "all";
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const input = document.getElementById("taskInput");
+const btn = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+btn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+  const task = {
+    text: taskText,
+    completed: false
+  };
+
+  tasks.push(task);
+  saveTasks();
+  renderTasks();
+
+  input.value = "";
+}
+
+ function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function updateTaskCount() {
+  const pending = tasks.filter(task => !task.completed).length;
+
+  document.getElementById("taskCount").innerText =
+    `${pending} task${pending !== 1 ? "s" : ""} left`;
+}
+function renderTasks() {
+  list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+  if (
+    currentFilter === "completed" && !task.completed ||
+    currentFilter === "pending" && task.completed
+  ) return;
+
+  const li = document.createElement("li");
+
+
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    li.innerHTML = `
+      <span class="task-text">${task.text}</span>
+      <div>
+         <button class="edit" data-index="${index}">✏️</button>
+        <button class="complete" data-index="${index}">✔️</button>
+        <button class="delete" data-index="${index}">❌</button>
+      </div>
+    `;
+
+    list.appendChild(li);
+  });
+  updateTaskCount();
+  toggleClearButton();
+}
+   function saveEdit(input, index) {
+  const newText = input.value.trim();
+
+  if (newText !== "") {
+    tasks[index].text = newText;
+  }
+
+  saveTasks();
+  renderTasks();
+}
+list.addEventListener("click", (e) => {
+  const button = e.target.closest("button");
+  if (!button) return;
+
+  const index = button.dataset.index;
+
+  if (button.classList.contains("delete")) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+  }
+
+  if (button.classList.contains("complete")) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks();
+    renderTasks();
+  }
+
+  if (button.classList.contains("edit")) {
+    const li = button.closest("li");
+    const span = li.querySelector(".task-text");
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = span.innerText;
+
+    li.replaceChild(input, span);
+    input.focus();
+
+    input.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        saveEdit(input, index);
+      }
+    });
+
+    input.addEventListener("blur", () => {
+      saveEdit(input, index);
+    });
+  }
+});
+
+  
+
+const filterButtons = document.querySelectorAll(".filters button");
+
+filterButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    currentFilter = btn.dataset.filter;
+     filterButtons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    renderTasks();
+  });
+});
+renderTasks();
+const clearBtn = document.getElementById("clearCompleted");
+
+clearBtn.addEventListener("click", () => {
+  tasks = tasks.filter(task => !task.completed);
+
+  saveTasks();
+  renderTasks();
+});
+function toggleClearButton() {
+  const hasCompleted = tasks.some(task => task.completed);
+  clearBtn.style.display = hasCompleted ? "block" : "none";
+}
